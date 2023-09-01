01de1d4e8d2b6703bfcca8e3026ac9b0c2a268e7
Shashank <shashwritescode@gmail.com>
2023-09-01 17:13:09 +0530

    Mapping New Components

--- verse_blog/verse/pages/index.js
+++ verse_blog/verse/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { PostCard, Categories, PostWidget } from "../components";
 
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing' },
@@ -7,26 +8,21 @@ const posts = [
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Verse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => (
-          <div className="lg:col-span-8 col-span-1">
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8">
-            
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => <PostCard post={post} key={post.title} />)}
         </div>
-
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
     </div>
   )
